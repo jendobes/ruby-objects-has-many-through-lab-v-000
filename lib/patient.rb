c13a32ff180ec6d24e17501eacb4ccc58828fe66
Jennifer Dobak <jdobak1@gmail.com>
2018-01-31 18:41:21 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,8 +7,14 @@ class Patient
   end
 
   def add_appointment(appointment)
-    @appointment << appointment
+    @appointments << appointment
     appointment.patient = self
+  end
+
+  def doctors
+    @appointments.collect do |appointment|
+      appointment.doctor
+    end
   end
 
 end
